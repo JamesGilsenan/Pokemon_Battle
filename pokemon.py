a497ccfdefc1f07b9dd1749362236d6d0e3c7c19
James Gilsenan <jamesgilsenan37@gmail.com>
2020-08-10 21:06:01 +0100

    Added lose_health method

--- pokemon.py
+++ pokemon.py
@@ -1,8 +1,22 @@
 class Pokemon:
-    def __init__(self, name, level, type, max_hp, current_hp, unconcious = false):
+    def __init__(self, name, level, type, max_hp, current_hp, unconcious = False):
         self.name = name
         self.level = level
         self.type = type
-        self.max_hp = max_hp + (2 * level)
-        self.current_hp = current_hp
+        self.max_hp = max_hp
+        self.current_hp = max_hp
         self.unconcious = unconcious
+
+    def lose_health(self, damage):
+        if damage <= 0:
+            print("{name} dodged the attack!".format(name = self.name))
+        elif damage >= self.current_hp:
+            self.current_hp -= damage
+            self.unconcious = True
+            print("{name} has taken {dmg} damage and is unable to continue".format(name=self.name, dmg = damage))
+        else:
+            print("{name} has taken {dmg} damage".format(name=self.name, dmg=damage))
+        
+
+pika = Pokemon("Pikachu", 1, "electric", 20, 20, False)
+pika.lose_health(10)
