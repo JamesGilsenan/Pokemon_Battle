1eb4aaf4531c8657642347a1c628f06307bd2192
James Gilsenan <jamesgilsenan37@gmail.com>
2020-08-12 14:34:36 +0100

    Added method set_health. Refactored poke methods

--- pokemon.py
+++ pokemon.py
@@ -1,47 +1,49 @@
 class Pokemon:
-    def __init__(self, name, level, type, max_hp, current_hp):
+    def __init__(self, name, level, type):
         self.name = name
         self.level = level
         self.type = type.lower()
-        self.max_hp = max_hp
-        self.current_hp = max_hp
+        self.max_hp = level + 3
+        self.current_hp = self.max_hp
         self.unconcious = False
+        self.xp = 0
 
-    def lose_health(self, damage
-    ):
-        def knocked_out(self):
+    def lose_health(self, damage):
+        self.current_hp -= damage
+        if self.current_hp <= 0:
             self.current_hp = 0
             self.unconcious = True
-            print("{name} has been knocked unconcious".format(name=self.name))
-
-        if damage <= 0:
-            print("{name} dodged the attack!".format(name = self.name))
-        elif damage >= self.current_hp:
-            self.current_hp -= 0
-            print("{name} has taken {dmg} damage. They have 0 hp".format(name=self.name, dmg = damage))
-            knocked_out(self)
+            print("{name} has taken {dmg} damage. {name} has been knocked unconcious!".format(name=self.name, dmg = damage))
         else:
-            self.current_hp -= damage
             print("{name} has taken {dmg} damage. They now have {hp} hp".format(name=self.name, dmg=damage,
              hp=self.current_hp))
     
     def regain_health(self, heal):
-        if self.unconcious is False:
+        if self.unconcious is False and self.current_hp < self.max_hp:
             self.current_hp += heal
-            if self.current_hp > self.max_hp:
-                self.current_hp = self.max_hp
+            self.current_hp = self.set_health()
             print("{name} was healed by {health} hp. They now have {hp} hp".format(name=self.name, health=heal, 
             hp=self.current_hp))
+        elif self.current_hp == self.max_hp:
+            print("Cannot heal {pokemon} as they already have full health".format(pokemon=self.name))
         else:
             print("Cannot heal {name}. They are unconcious".format(name=self.name))
 
     def revive(self, health):
-        if health >= self.max_hp:
-            self.current_hp = self.max_hp
-            self.unconcious = False
+        if self.unconcious is False:
+            print("Cannot revive {pokemon} because they are already concious".format(pokemon=self.name))
         else:
             self.current_hp = health
-        print("{name} was revived. They now have {hp} hp".format(name=self.name, hp=self.current_hp))
+            self.unconcious = False
+            print("{name} was revived. They now have {hp} hp".format(name=self.name, hp=self.current_hp))
+
+    def set_health(self):
+        if self.current_hp >= self.max_hp:
+            return self.max_hp
+        elif self.current_hp <= 0:
+            return 0
+        else:
+            return self.current_hp
 
     def attack(self, other_pokemon):
         if self.unconcious is True:
@@ -75,6 +77,9 @@ class Pokemon:
             print("{name}'s attack wasn't very effective".format(name=self.name))
         else:
             other_pokemon.lose_health(self.level)
+            #if other_pokemon.lose_health == True:
+            #    self.xp += other_pokemon.level
+            #    print("{pokemon} gained {xp} xp".format(pokemon=self.name, xp=self.xp))        
 
 
 class Trainer():
@@ -105,25 +110,28 @@ class Trainer():
 
 
 
-pika = Pokemon("Pikachu", 2, "Electric", 20, 20)
-squirtle = Pokemon("Squirtle", 1, "Water", 15, 15)
-bulbasour = Pokemon("Bulbasour", 3, "Grass", 30, 30)
-charmander = Pokemon("Charmander", 4, "fire", 35, 35)
-starme = Pokemon("Starme", 3, "water", 30, 30)
-ash = Trainer("Ash", [pika, charmander, bulbasour], 3, 0)
-misty = Trainer("Misty", [starme, squirtle], 3, 0)
+pika = Pokemon("Pikachu", 2, "Electric")
+#squirtle = Pokemon("Squirtle", 1, "Water", 15, 15)
+#bulbasour = Pokemon("Bulbasour", 3, "Grass", 30, 30)
+#charmander = Pokemon("Charmander", 4, "fire", 35, 35)
+starme = Pokemon("Starme", 3, "water")
+#ash = Trainer("Ash", [pika, charmander, bulbasour], 3, 0)
+#misty = Trainer("Misty", [starme, squirtle], 3, 0)
+ash = Trainer("Ash", [pika], 3, 0)
+misty = Trainer("Misty", [starme], 3, 0)
 
-#pika.lose_health(25)
-#pika.regain_health(5)
+pika.lose_health(5)
+pika.revive(pika.max_hp)
 #pika.revive(50)
 #pika.attack(squirtle)
 #squirtle.attack(charmander)
 #bulbasour.attack(pika)
 #charmander.attack(squirtle)
-bulbasour.lose_health(50)
+#bulbasour.lose_health(50)
 #ash.use_potion()
-ash.attack_trainer(misty)
-misty.switch_pokemon(1)
-misty.attack_trainer(ash)
-ash.attack_trainer(misty)
-ash.switch_pokemon(2)
+#ash.attack_trainer(misty)
+#misty.switch_pokemon(1)
+#misty.attack_trainer(ash)
+#ash.attack_trainer(misty)
+#ash.switch_pokemon(2)
+
